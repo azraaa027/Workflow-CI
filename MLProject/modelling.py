7a5984b9061a370e9bf8ab77dfaced6c991d0f37
Mohamad Azra Muntaha <moazraaa@gmail.com>
2025-12-24 21:26:15 +0700

    Initial commit9

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,11 +1,10 @@
-# modelling.py (versi kompatibel dengan mlflow run)
+# modelling.py
 import mlflow
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# ✅ MLflow otomatis set tracking URI ke ./mlruns/
 mlflow.set_experiment("titanic-ci")
 
 df = pd.read_csv("titanic_preprocessed.csv")
@@ -13,7 +12,6 @@ X = df.drop("Survived", axis=1)
 y = df["Survived"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# ✅ Autolog → hasilkan artefak lengkap
 mlflow.sklearn.autolog(log_models=True)
 
 with mlflow.start_run():
@@ -22,5 +20,4 @@ with mlflow.start_run():
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     mlflow.log_metric("test_accuracy", acc)
-    
-    print(f"✅ CI training selesai. Accuracy: {acc:.4f}")
+    print(f"✅ Training selesai. Accuracy: {acc:.4f}")
